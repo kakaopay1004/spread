7946b013b76729c8d504f3f920ed363175ea3060
최규선/플랫폼개발팀 <ks.choi@musinsa.com>
2020-06-26 18:49:04 +0900

    TokenUtil component

--- src/main/java/com/kakaopay/spread/service/SpreadService.java
+++ src/main/java/com/kakaopay/spread/service/SpreadService.java
@@ -4,11 +4,10 @@ import com.kakaopay.spread.entity.Spread;
 import com.kakaopay.spread.entity.SpreadDetail;
 import com.kakaopay.spread.exception.HttpNotFoundException;
 import com.kakaopay.spread.repository.SpreadRepository;
+import com.kakaopay.spread.util.TokenUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.Random;
 
 @Slf4j
 @Service
@@ -16,19 +15,11 @@ import java.util.Random;
 public class SpreadService {
 
     private final SpreadRepository spreadRepository;
+    private final TokenUtil tokenUtil;
 
     public void create(Spread spread) {
-        spread.setToken(createRandomToken());
+        spread.setToken(tokenUtil.createRandomToken());
 
-        addSpreadDetails(spread);
-
-        spreadRepository.save(spread);
-
-        log.info("spread getCreateDate : {}, getModifiedDate : {}", spread.getCreateDate(), spread.getModifiedDate());
-        log.info("spread token : {}", spread.getToken());
-    }
-
-    private void addSpreadDetails(Spread spread) {
         double spreadMoney = spread.getMoney();
 
         for (int i = 0; i < spread.getCount(); i++) {
@@ -42,40 +33,19 @@ public class SpreadService {
                 spreadDetail.setMoney(money);
             }
 
-            log.info("money : {}, price : {}", spreadDetail.getMoney(), spreadMoney);
             spreadDetail.setSpread(spread);
             spread.getSpreadDetails().add(spreadDetail);
         }
 
-    }
+        spreadRepository.save(spread);
 
-    private String createRandomToken() {
-        StringBuilder stringbuilder = new StringBuilder();
-        Random random = new Random();
-        for (int i = 0; i < 3; i++) {
-            int randomInt = random.nextInt(3);
-            switch (randomInt) {
-                case 0:
-                    // a-z
-                    stringbuilder.append((char) ((int) (Math.random() * 26) + 97));
-                    break;
-                case 1:
-                    // A-Z
-                    stringbuilder.append((char) ((int) (Math.random() * 26) + 65));
-                    break;
-                case 2:
-                    // 0-9
-                    stringbuilder.append(random.nextInt(10));
-                    break;
-            }
-        }
-        return stringbuilder.toString();
     }
 
     public Spread findByRoomIdAndToken(String roomId, String token) {
 
         return spreadRepository.findByRoomIdAndToken(roomId, token)
                 .orElseThrow(HttpNotFoundException::new);
+
     }
 
 }
